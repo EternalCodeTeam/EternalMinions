8faecf31b1f144ba998ee7f23fe075324ab5ca61
Martin Sulikowski <vluckyyy.biznes@gmail.com>
2026-07-24 20:43:51 +0200

    Use enum for give command.

--- eternalminions-plugin/src/main/java/com/eternalcode/minions/command/MinionGiveCommand.java
+++ eternalminions-plugin/src/main/java/com/eternalcode/minions/command/MinionGiveCommand.java
@@ -2,6 +2,7 @@ package com.eternalcode.minions.command;
 
 import com.eternalcode.minions.config.MessagesConfig;
 import com.eternalcode.minions.item.MinionItemFactory;
+import com.eternalcode.minions.minion.MinionBehaviorType;
 import com.eternalcode.minions.minion.MinionType;
 import com.eternalcode.minions.minion.MinionTypeService;
 import com.eternalcode.multification.notice.Notice;
@@ -10,6 +11,7 @@ import dev.rollczi.litecommands.annotations.command.Command;
 import dev.rollczi.litecommands.annotations.context.Context;
 import dev.rollczi.litecommands.annotations.execute.Execute;
 import dev.rollczi.litecommands.annotations.permission.Permission;
+import java.util.Locale;
 import org.bukkit.entity.Player;
 
 @Command(name = "minions")
@@ -33,12 +35,12 @@ public final class MinionGiveCommand {
 
     @Execute(name = "give")
     @Permission("eternalminions.command.give")
-    public Notice give(@Context Player player, @Arg("typ") String typeId) {
-        MinionType type = this.types.type(typeId).orElse(null);
-        if (type == null) {
+    public Notice give(@Context Player player, @Arg("type") MinionBehaviorType type) {
+        MinionType minionType = this.types.type(type.name().toLowerCase(Locale.ROOT)).orElse(null);
+        if (minionType == null) {
             return this.messages.minionTypeUnknown;
         }
-        return this.give(player, type);
+        return this.give(player, minionType);
     }
 
     private Notice give(Player player, MinionType type) {
